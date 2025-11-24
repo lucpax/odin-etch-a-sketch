e2faf15b12cd2b2b5d9d184a7d8d76437b0da76d
Lucas <80587273+lucpax@users.noreply.github.com>
2025-11-24 17:36:03 -0300

    add new grid button

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const container = document.querySelector("div")
-container.style.cssText = "display: flex; width: 800px; flex-wrap: wrap"
+
+container.style.cssText = "display: flex; width: 960px; flex-wrap: wrap"
 for ( let i = 1; i < 257; i++) {
     const grid = document.createElement("div")
-grid.style.cssText = "width: 50px; height: 50px; border: 1px solid black"
+grid.style.cssText = "width: 60px; height: 60px; border: 1px solid black"
 grid.addEventListener("mouseover", () => {grid.style.backgroundColor = "blue"})
 container.appendChild(grid)
 }
+
+const button = document.createElement("button")
+button.textContent = "New grid"
+button.addEventListener("click", () => {
+    let numberSquares = parseInt(prompt("How many squares per side?"))
+})
+
+container.appendChild(button)
