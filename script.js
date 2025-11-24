b960352439556d03045e4018f814384c0fd655cf
Lucas <80587273+lucpax@users.noreply.github.com>
2025-11-24 17:52:39 -0300

    add function to create grid

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.querySelector("div")
 
+function createGrid(numberSquares) {
+    let squareWidth = 960/numberSquares
 container.style.cssText = "display: flex; width: 960px; flex-wrap: wrap"
-for ( let i = 1; i < 257; i++) {
+for ( let i = 0; i < numberSquares * numberSquares; i++) {
     const grid = document.createElement("div")
-grid.style.cssText = "width: 60px; height: 60px; border: 1px solid black"
+grid.style.cssText = `width: ${squareWidth}px; height: ${squareWidth}px; border: 1px solid black`
 grid.addEventListener("mouseover", () => {grid.style.backgroundColor = "blue"})
 container.appendChild(grid)
 }
-
-const button = document.createElement("button")
+}
+const button = document.querySelector("button")
 button.textContent = "New grid"
 button.addEventListener("click", () => {
     let numberSquares = parseInt(prompt("How many squares per side?"))
+    if (numberSquares > 0 && numberSquares <= 100) {
+        container.innerHTML = ""
+        createGrid(numberSquares)
+    }
+    else {
+        alert("try again")
+    } 
 })
 
-container.appendChild(button)
+createGrid(16)
